20814cb4e325df9df379442643adb19a07856cf6
Nyany <yunyikarashi@gmail.com>
2025-11-14 09:44:03 +0900

    change vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
+    base: process.env.NODE_ENV === 'production' ? '/nyany/' : '/',
   ],
   resolve: {
     alias: {
